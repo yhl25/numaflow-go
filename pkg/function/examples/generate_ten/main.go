3a2f1b1ba6e8b96a7558e7a191036ec016ffc143
jyu6 <juanlu_yu@intuit.com>
2023-03-10 10:28:31 -0800

    add lock
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/function/examples/generate_ten/main.go
+++ pkg/function/examples/generate_ten/main.go
@@ -3,21 +3,31 @@ package main
 import (
 	"context"
 	"strconv"
+	"sync"
 
 	functionsdk "github.com/numaproj/numaflow-go/pkg/function"
 	"github.com/numaproj/numaflow-go/pkg/function/server"
 )
 
 var genKey = 0
+var keyLock sync.RWMutex
 
 func handle(_ context.Context, key string, d functionsdk.Datum) functionsdk.Messages {
 	msg := d.Value()
 	_ = d.EventTime() // Event time is available
 	_ = d.Watermark() // Watermark is available
+	keyLock.RLock()
 	if genKey == 10 {
+		keyLock.RUnlock()
+		keyLock.Lock()
 		genKey = 0
+		keyLock.Unlock()
+	} else {
+		keyLock.RUnlock()
 	}
+	keyLock.Lock()
 	genKey++
+	keyLock.Unlock()
 	return functionsdk.MessagesBuilder().Append(functionsdk.MessageTo(strconv.Itoa(genKey), msg))
 }
 
